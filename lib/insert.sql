fdec39543197ce16ae4cc6af4e8f1db511bd6357
Vanna Winland <vgwinland@gmail.com>
2019-11-10 22:51:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,8 @@
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'calm', 0);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Rowdy', 3, 'F', 'Light Brown', 'rowdy', 0);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Tabitha', 12, 'F', 'Medium Brown', 'calm', 0);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'mild', 1);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 7, 'F', 'Light Brown', 'goofy', 0);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Grinch', 15, 'M', 'Green', 'grumpy', 0);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Wendy', 20, 'F', 'Brown', 'angry', 1);
-INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES (NULL, 50, 'F', 'Light Brown', 'protective', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Cuddly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Crazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 8, "F", "Grizzly", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "Loud", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Grizzly", "Loving", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "Sluggish", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "M", "Grizzly", "Hungry", 0);
