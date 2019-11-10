c6356d75c8a2184d3fa1e29ce94a7170a188b2fd
Vanna Winland <vgwinland@gmail.com>
2019-11-10 22:48:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'calm', '0');
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'calm', 0);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Rowdy', 3, 'F', 'Light Brown', 'rowdy', '0');
+VALUES ('Rowdy', 3, 'F', 'Light Brown', 'rowdy', 0);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Tabitha', 12, 'F', 'Medium Brown', 'calm', '0');
+VALUES ('Tabitha', 12, 'F', 'Medium Brown', 'calm', 0);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'mild', '1');
+VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'mild', 1);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 7, 'F', 'Light Brown', 'goofy', '0');
+VALUES ('Melissa', 7, 'F', 'Light Brown', 'goofy', 0);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Grinch', 15, 'M', 'Green', 'grumpy', '0');
+VALUES ('Grinch', 15, 'M', 'Green', 'grumpy', 0);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Wendy', 20, 'F', 'Brown', 'angry', '1');
+VALUES ('Wendy', 20, 'F', 'Brown', 'angry', 1);
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES (NULL, 50, 'F', 'Light Brown', 'protective', '0');
+VALUES (NULL, 50, 'F', 'Light Brown', 'protective', 0);
