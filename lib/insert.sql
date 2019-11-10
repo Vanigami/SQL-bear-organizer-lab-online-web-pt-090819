0d3b3c93362d3099eb6a3e5da513a0e73eb2dce0
Vanna Winland <vgwinland@gmail.com>
2019-11-10 22:24:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,7 @@ VALUES ('Tabitha', 12, 'F', 'Medium Brown', 'calm', '0');
 INSERT INTO (name, age, gender, color, temperament, alive)
 VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'mild', '1');
 INSERT INTO (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 7, 'F', 'Light Brown', 'curious', '0');
+VALUES ('Melissa', 7, 'F', 'Light Brown', 'goofy', '0');
 INSERT INTO (name, age, gender, color, temperament, alive)
 VALUES ('Grinch', 15, 'M', 'Green', 'grumpy', '0');
 INSERT INTO (name, age, gender, color, temperament, alive)
